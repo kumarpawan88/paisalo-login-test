b41d8efc47e303ef0f6ad0e284d0827d90899fa9
kumarpawan88 <office.kumar.01001@gmail.com>
2025-08-05 03:14:05 +0000

    work_book.ts

---
+++ work_book.ts
@@ -0,0 +1,6 @@
+class mom{ name: string; 
+    constructor(name: string) {this.name= name; }
+    greet(): void{console.log(`Hello, ${this.name}!`);}}
+    
+    const I = new mom('Momy');
+    I.greet(); 
